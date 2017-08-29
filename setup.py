42088242f3d735c17e0f5a0b74d4ef98bf9af905
roberttk01 <roberttk01@gmail.com>
2017-08-29 12:54:26 -0500

    Updated setup.py to include basic information

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-
-setup(console=['./pystructpro/pystructpro.py'])
+setup(
+    name="PyStructPro",
+    version="1.0",
+    description="A small project used to generate directory skeleton for personal projects",
+    url="https://github.com/roberttk01/pystructpro.git",
+    author="Thomas Kyle Robertson",
+    author_email="roberttk01@gmail.com",
+    license="MIT",
+    packages=["pystructpro"],
+)
